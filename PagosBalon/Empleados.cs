b0ccdc3729acaebdd856704d7c100c9b3e1341a8
Ludswing Cahuec <ludswingmichael@outlook.com>
2018-11-12 01:02:38 -0600

    Agregar archivos de proyecto.

---
+++ PagosBalon/Empleados.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagosBalon
+{
+    class Empleados : IFabrica
+    {
+        int cantBalones;
+        string  nombre;
+        double precio;
+        double comision;
+        double t,tFinal;
+
+        public int CantBalones
+        {
+            get { return cantBalones; }
+            set { cantBalones = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public double Precio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+        public double Comision
+        {
+            get { return comision; }
+            set { comision = value; }
+        }
+
+        //Craecion del constructor de la clase
+        public Empleados(int CANT, string NOMBRE, double PRECIO, double COMISIONES)
+        {
+            this.cantBalones = CANT;
+            this.nombre = NOMBRE;
+            this.precio = PRECIO;
+            this.comision = COMISIONES;
+            
+        }
+
+        //CReacion de la Segregacion de la interfaz
+        public void pagos()
+        {
+            t = precio * comision;
+            tFinal = t * cantBalones;
+
+            Console.WriteLine("Nombre del Empleado: " + nombre + "\nBalones hechos: " +cantBalones + "\nSalario: Q " +tFinal);
+        }
+
+    }
+}
---
+++ PagosBalon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagosBalon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleados Empleado1 = new Empleados(100,"Ludswing",25,2);
+            Empleado1.pagos();
+            Console.ReadKey();
+        }
+    }
+}
